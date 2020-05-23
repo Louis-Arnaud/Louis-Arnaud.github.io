a3df8ec4189b435131a185c5ede1abbdb7e4a8fb
BOUQUIN Louis-Arnaud <lab@pmconseil.com>
2020-05-23 19:25:15 +0000

    Ajout lien twitter

--- controller/Main.controller.js
+++ controller/Main.controller.js
@@ -2,9 +2,14 @@ sap.ui.define(["sap/ui/core/mvc/Controller"], function (Controller) {
 	"use strict";
 	return Controller.extend("lab.PresentationSite.controller.Main", {
 		onInit: function () {},
-		
 		goToGithubPage: function (oEvent) {
 			window.location.replace("https://github.com/Louis-Arnaud");
+		},
+		goToSapProfile: function (oEvent) {
+			window.location.replace("https://people.sap.com/lab_pm");
+		},
+		goToTwitterProfile: function (oEvent) {
+			window.location.replace("https://twitter.com/LAB_SAP");
 		}
 	});
 });
