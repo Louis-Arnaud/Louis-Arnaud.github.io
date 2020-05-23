996916c9bed03114d6240d8ce9eb7c5194f88222
BOUQUIN Louis-Arnaud <lab@pmconseil.com>
2020-05-23 17:50:39 +0000

    Ajout lien vers github

--- controller/Main.controller.js
+++ controller/Main.controller.js
@@ -1,11 +1,10 @@
-sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+sap.ui.define(["sap/ui/core/mvc/Controller"], function (Controller) {
 	"use strict";
-
 	return Controller.extend("lab.PresentationSite.controller.Main", {
-		onInit: function () {
-
+		onInit: function () {},
+		
+		goToGithubPage: function (oEvent) {
+			window.location.replace("https://github.com/Louis-Arnaud");
 		}
 	});
 });
