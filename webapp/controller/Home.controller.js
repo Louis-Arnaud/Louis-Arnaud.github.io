fce92136c48fbbad8e20b11c78a51532cab2500c
louis-arnaud-bouquin <louis-arnaud.bouquin@viseo.com>
2024-10-14 15:26:41 +0200

    mise en forme "Card"

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -1,14 +1,72 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"../model/formatter"
-], function (Controller, formatter) {
+	"../model/formatter",
+	"sap/ui/model/json/JSONModel",
+	"sap/ui/core/dnd/DragInfo",
+	"sap/f/dnd/GridDropInfo",
+	"sap/ui/core/library"
+], function (Controller, formatter, JSONModel, DragInfo, GridDropInfo, coreLibrary) {
 	"use strict";
 
-	return Controller.extend("lab.PresentationSite.controller.App", {
+	return Controller.extend("lab.PresentationSite.controller.Home", {
 
 		formatter: formatter,
 
 		onInit: function () {
+
+			var oCardManifests = new JSONModel(sap.ui.require.toUrl("lab/PresentationSite/cardManifest.json"));
+			this.getView().setModel(oCardManifests, "manifests");
+
+			// shortcut for sap.ui.core.dnd.DropLayout
+			var DropLayout = coreLibrary.dnd.DropLayout;
+
+			// shortcut for sap.ui.core.dnd.DropPosition
+			var DropPosition = coreLibrary.dnd.DropPosition;
+
+			var oGrid = this.byId("idGridContainer");
+
+			oGrid.addDragDropConfig(new DragInfo({
+				sourceAggregation: "items"
+			}));
+
+			oGrid.addDragDropConfig(new GridDropInfo({
+				targetAggregation: "items",
+				dropPosition: DropPosition.Between,
+				dropLayout: DropLayout.Horizontal,
+				drop: function (oInfo) {
+					var oDragged = oInfo.getParameter("draggedControl"),
+						oDropped = oInfo.getParameter("droppedControl"),
+						sInsertPosition = oInfo.getParameter("dropPosition"),
+						iDragPosition = oGrid.indexOfItem(oDragged),
+						iDropPosition = oGrid.indexOfItem(oDropped);
+
+					oGrid.removeItem(oDragged);
+
+					if (iDragPosition < iDropPosition) {
+						iDropPosition--;
+					}
+
+					if (sInsertPosition === "After") {
+						iDropPosition++;
+					}
+
+					oGrid.insertItem(oDragged, iDropPosition);
+					oGrid.focusItem(iDropPosition);
+				}
+			}));
+
+			// Use smaller margin around grid when on smaller screens
+			oGrid.attachLayoutChange(function (oEvent) {
+				var sLayout = oEvent.getParameter("layout");
+
+				if (sLayout === "layoutXS" || sLayout === "layoutS") {
+					oGrid.removeStyleClass("sapUiSmallMargin");
+					oGrid.addStyleClass("sapUiTinyMargin");
+				} else {
+					oGrid.removeStyleClass("sapUiTinyMargin");
+					oGrid.addStyleClass("sapUiSmallMargin");
+				}
+			});
 
 		},
 
--- webapp/model/formatter.js
+++ webapp/model/formatter.js
@@ -2,5 +2,14 @@ sap.ui.define([], function () {
 	"use strict";
 	return {
 
+		calculateAge: function(birthday) { // birthday is a date
+
+			var birthDate = Date.parse(birthday);
+
+			var ageDifMs = Date.now() - birthDate;
+			var ageDate = new Date(ageDifMs); // miliseconds from epoch
+			return Math.abs(ageDate.getUTCFullYear() - 1970);
+		}
+
 	};
 });
